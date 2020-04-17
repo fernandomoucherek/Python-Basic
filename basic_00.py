a0087d044cd0c4f380ca5a619f92b6f24239c8eb
fernandomoucherek <fernando@fisica.uema.br>
2020-04-16 22:41:58 -0300

    basic_00.py

--- basic_00.py
+++ basic_00.py
@@ -1,7 +1,28 @@
 print('Hello')
 
-# Operaçoes Aritiméticas
-# + adição
-# - subtração
-# * multiplicação 
-# / divisão
+# Operacoes Aritimeticas
+# + add
+# - subtraction
+# * multiply
+# / division
+
+#Exemple - 1
+
+# 2 + 3 
+print(2 + 3)
+
+# 2 - 3 
+print(2 - 3)
+
+# 2*3
+print(2*3)
+
+#2/3
+print(2/3)
+print(2./3.)
+
+# 1 + 2 * 3
+print(1 + 2 * 3)
+
+# (1 + 2) * 3
+print((1 + 2) * 3)
